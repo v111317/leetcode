626010a2211c26487f7cda7a797b5846df3191d5
v111317 <147157983+v111317@users.noreply.github.com>
2024-02-14 23:56:58 +0530

    adding - unsolved - search-positio and stock

---
+++ leetcode_problems/in_progress/best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,7 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        a = 1
+        isDecreasing = True
+        
+        
+        
--- leetcode_problems/in_progress/search-insert-position.py
+++ leetcode_problems/in_progress/search-insert-position.py
@@ -6,17 +6,30 @@ class Solution:
         startIdx = 0
         endIdx = len(nums) - 1
         midIdx = math.ceil(endIdx/2)
+        
         while midIdx < endIdx:
+            if nums[startIdx]==target:
+                return startIdx
+            elif nums[endIdx]==target:
+                return endIdx
+            elif nums[midIdx]==target:
+                return midIdx
+                
             if target < nums[midIdx]:
                 endIdx = midIdx
                 midIdx = math.ceil((startIdx+midIdx)/2)
             elif target > nums[midIdx]:
                 startIdx = midIdx
                 midIdx = math.ceil((midIdx+endIdx)/2)
-            else:
-                return midIdx
         
-        return midIdx    
+        if target > nums[midIdx]:
+            return midIdx+1
+        elif target < nums[midIdx]:
+            return midIdx
+                    
+          
     
 sol1 = Solution()
-print(sol1.searchInsert([1,3,5,6], 1))
+print(sol1.searchInsert([1, 3, 5, 6], 3))
+print(sol1.searchInsert([1, 3, 5, 6], 2))
+print(sol1.searchInsert([1, 3, 5, 6], 0))
