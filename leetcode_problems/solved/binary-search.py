7d19169c54e40328ad9b6bbd7eacf6e168ef294b
v111317 <147157983+v111317@users.noreply.github.com>
2024-02-25 08:41:16 +0530

    adding- solved:1

---
+++ leetcode_problems/solved/binary-search.py
@@ -0,0 +1,34 @@
+#https://leetcode.com/problems/binary-search/description/
+
+# Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. 
+# If target exists, then return its index. Otherwise, return -1.
+
+# You must write an algorithm with O(log n) runtime complexity.
+from typing import List
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if len(nums)==1:
+            if target in nums:
+                return 0
+            else:
+                return -1
+        
+        start = 0
+        end = len(nums)-1
+        mid = end // 2
+        
+        while start <= end:
+            print(start, mid, end)
+            if target > nums[mid]:
+                start = mid + 1
+            elif target < nums[mid]:
+                end = mid - 1
+            else:
+                return mid
+            mid = (end+start)//2
+        return -1
+    
+sol1 = Solution()
+print(sol1.search([-1,0,3,5,9,12], 8))
+print(sol1.search([2,5], 5))
